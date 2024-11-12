188ffebd3ba499aff2632f679299d092a0e7083d
Amanda Mensah-Biney <airlinepilotamanda@gmail.com>
2024-11-12 00:46:51 +0000

    argghh

--- controllers/user.js
+++ controllers/user.js
@@ -82,21 +82,26 @@ export const loginUser = async (req, res) => {
 
 export const getUserProfile = async (req, res) => {
   try {
-    const userId = req.user.id; // Access the user ID here
+    const userId = req.user.id; // Access the ID from the decoded token
 
-    // Fetch user data from the database using userId
-    const user = await User.findById(userId);
-
+    const user = await User.findById(userId); // Retrieve user from the database
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
 
-    res.status(200).json(user);
+    res.status(200).json({
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      contact: user.contact,
+      location: user.location,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Server error' });
   }
 };
+
 
 
 export const updateUserProfile = async (req, res) => {
