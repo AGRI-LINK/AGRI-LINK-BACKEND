c52c768211385557fb8a489d56dc7f337b32abc6
Amanda Mensah-Biney <airlinepilotamanda@gmail.com>
2024-11-14 12:02:06 +0000

    one more

--- models/products.js
+++ models/products.js
@@ -4,8 +4,8 @@ import mongoose from 'mongoose';
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },          
   description: { type: String },                   
-  quantity: { type: Number, required: true },      
-  price: { type: Number, required: true },         
+  quantity: { type: String, required: true },      
+  price: { type: String, required: true },         
   category: { type: String, required: true },      
   location: { type: String },                      
   images: [{ type: String }],                      
