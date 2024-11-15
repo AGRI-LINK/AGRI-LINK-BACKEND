034f347ae5a2085e44d82e8f91f99cc83412788b
Amanda Mensah-Biney <airlinepilotamanda@gmail.com>
2024-11-15 14:02:11 +0000

    hakuna matata

--- routes/products.js
+++ routes/products.js
@@ -8,18 +8,18 @@ import authorizeFarmer from '../middlewares/authorizeFarmer.js';
 const router = express.Router();
 
 
-router.post('/products/add', authenticate, authorizeFarmer, addNewProduct)
+router.post('/add', authenticate, authorizeFarmer, addNewProduct)
 
-router.patch('/products/update/:id', authenticate,  authorizeFarmer, updateProductById);
+router.patch('/update/:id', authenticate,  authorizeFarmer, updateProductById);
 
-router.delete('/products/delete/:id', authenticate, authorizeFarmer, deleteProductById);
+router.delete('/delete/:id', authenticate, authorizeFarmer, deleteProductById);
 
-router.delete('/products/delete', authenticate, authorizeFarmer, deleteAllProducts);
+router.delete('/delete', authenticate, authorizeFarmer, deleteAllProducts);
 
 
-router.get('/products/list', getAndFilterProducts);
+router.get('/list', getAndFilterProducts);
 
-router.get('/products/get/:id', authenticate, authorizeFarmer, getProductById);
+router.get('/get/:id', authenticate, authorizeFarmer, getProductById);
 
 export default router;
 
