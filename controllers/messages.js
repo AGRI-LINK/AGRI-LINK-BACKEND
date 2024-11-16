e7546b5c633157b6e0d0c879646fcfcdba186cf1
Amanda Mensah-Biney <airlinepilotamanda@gmail.com>
2024-11-16 20:43:47 +0000

    langa

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,7 +1,5 @@
 import Message from '../models/messages.js';
-import User from '../models/user.js';
 
-import sendEmail from '../utils/sendEmail.js';
 
 export const sendMessage = async (req, res) => {
   const { receiverId, content } = req.body;
@@ -19,23 +17,11 @@ export const sendMessage = async (req, res) => {
 
     await message.save();
 
-    // Fetch receiver details to send email
-    const receiver = await User.findById(receiverId);
-    if (receiver) {
-      sendEmail(
-        receiver.email,
-        'New Message on AgriLink',
-        `You have received a new message from ${req.user.name}: "${content}"`
-      );
-    }
-
     res.status(201).json({ message: 'Message sent successfully!', messageData: message });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
-
-
 
 export const getInboxMessages = async (req, res) => {
   try {
