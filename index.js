de41da0bbcd33dd53d58a8a42ea8dd271e742548
Amanda Mensah-Biney <airlinepilotamanda@gmail.com>
2024-11-19 10:05:06 +0000

    let it work

--- index.js
+++ index.js
@@ -22,9 +22,13 @@ import reviewsRouter from './routes/reviews.js';
 import subscriptionRoutes from './routes/subscription.js';
 
 import uploadRoute from './routes/upload.js'; // Import upload route
+import { fileURLToPath } from 'url';
 import path from 'path';
 
 
+// Manually define __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 import cors from 'cors';
 
