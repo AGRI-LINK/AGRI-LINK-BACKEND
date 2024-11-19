d47077a1099af6ff2dbdcffd64ff2eb9fd4a8a51
Amanda Mensah-Biney <airlinepilotamanda@gmail.com>
2024-11-19 10:39:25 +0000

    file

--- routes/upload.js
+++ routes/upload.js
@@ -5,7 +5,7 @@ import upload from '../middlewares/upload.js';
 const router = express.Router();
 
 // Route to handle single image uploads
-router.post('/upload', upload.single('image'), (req, res) => {
+router.post('/upload', upload.single('file'), (req, res) => {
     try {
       res.status(200).json({
         message: 'Image uploaded successfully!',
